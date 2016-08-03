98e61c56a2c10374f50988567f6959cc245d4e0e
Danny <dapahu@t-online.de>
2016-08-03 19:42:50 +0200

    xcode commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world!" vom xcode<< endl;
         cout << "Hello world! the second one" << endl;
     cout << "yet another test";
     return 0;
